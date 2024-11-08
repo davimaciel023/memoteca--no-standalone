efd8ef686b3c9257ea16e2d442498d657263f3c2
Davi Maciel <davimaciel077@gmail.com>
2024-11-07 22:02:30 -0300

    commit

--- src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -1,6 +1,7 @@
 import { PensamentoService } from './../pensamento.service';
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -15,7 +16,10 @@ export class ListarPensamentoComponent implements OnInit {
   filtro: string = ''
   favoritos: boolean = false
   listaFavoritos: Pensamento[] = []
-  constructor(private service: PensamentoService) { }
+  constructor(
+    private service: PensamentoService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
     this.service.listar(this.paginaAtual, this.filtro, this.favoritos).subscribe((listaPensamentos) => {
@@ -39,6 +43,14 @@ export class ListarPensamentoComponent implements OnInit {
     this.service.listar(this.paginaAtual, this.filtro, this.favoritos).subscribe((listarPensamentos) => {
       this.listaPensamentos = listarPensamentos
     })
+  }
+
+  recarregaComponent() {
+    this.favoritos = false;
+    this.paginaAtual = 1;
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false
+    this.router.onSameUrlNavigation = 'reload'
+    this.router.navigate([this.router.url])
   }
 
   listarFavoritos() {
